a51a322727b7db0a1c8a18835be166edeea7aa48
Vishal Pandey <vishalpandey@Vishals-MacBook-Air.local>
2025-09-06 20:08:41 +0530

    updated rg module

---
+++ Modules/azurerm_rg/variable.tf
@@ -0,0 +1,10 @@
+variable "rg_name" {
+  description = "The name of the resource group"
+  type        = string
+  
+}
+
+variable "location" {
+  description = "The location/region where the resource group will be created"
+  type        = string
+}
